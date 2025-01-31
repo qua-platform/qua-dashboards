3fc59f9d730978e7e6e9c633d3de97e0d1bc60ac
Maxim V4S <maxim.d@v4scale.com>
2025-01-31 19:05:44 +0300

    check

--- src/qua_dashboards/app.py
+++ src/qua_dashboards/app.py
@@ -1,25 +1,48 @@
 import dash
-from dash import Dash, html
+from dash import Dash, html, dcc
 from qua_dashboards.logging_config import logger
 from qua_dashboards.pages import dashboards_registry
 import dash_bootstrap_components as dbc
+
 
 
 app = Dash(
     __name__,
     use_pages=True,
     external_stylesheets=[dbc.themes.BOOTSTRAP],
+    url_base_pathname="/dashboards/",
 )
 
+dash.register_page("another_home", layout=html.Div("We're home!"), path="/other-path")
+dash.register_page(
+    "very_important", layout=html.Div("Don't miss it!"), path="/important", order=0
+)
 
-for key, dashboard in dashboards_registry.items():
-    logger.info(f"Setting up dashboard: {key}")
-    dashboard.setup_api(app=app)
-    dashboard.setup_callbacks(app=app)
+# for key, dashboard in dashboards_registry.items():
+#     logger.info(f"Setting up dashboard: {key}")
+#     dashboard.setup_api(app=app)
+#     dashboard.setup_callbacks(app=app)
 
-
+# app.layout = html.Div(
+#     [
+#         dash.page_container,
+#     ]
+# )
 app.layout = html.Div(
     [
+        html.H1("App Frame"),
+        html.Div(
+            [
+                html.Div(
+                    dcc.Link(
+                        f"{page['name']} - {page['path']}", href=(page["relative_path"])
+                    )
+                )
+                for page in dash.page_registry.values()
+                if page["module"] != "pages.not_found_404"
+            ]
+        ),
+        html.Hr(),
         dash.page_container,
     ]
 )
--- src/qua_dashboards/pages/data_dashboard.py
+++ src/qua_dashboards/pages/data_dashboard.py
@@ -1,5 +1,5 @@
 import dash
-from qua_dashboards.data_dashboard.app import DataDashboardApp
+# from qua_dashboards.data_dashboard.app import DataDashboardApp
 from qua_dashboards.logging_config import logger
 
 logger.info("Registering page data-dashboard")
@@ -8,10 +8,14 @@ logger.info("Registering page data-dashboard")
 dash.register_page(__name__)
 
 
-data_dashboard_app = DataDashboardApp()
-layout = data_dashboard_app.app.layout
+layout = dash.html.Div(
+    dash.html.H1("Data Dashboard"),
+)
 
-
-from qua_dashboards.pages import dashboards_registry
-
-dashboards_registry["data-dashboard"] = data_dashboard_app
+# data_dashboard_app = DataDashboardApp()
+# layout = data_dashboard_app.app.layout
+#
+#
+# from qua_dashboards.pages import dashboards_registry
+#
+# dashboards_registry["data-dashboard"] = data_dashboard_app

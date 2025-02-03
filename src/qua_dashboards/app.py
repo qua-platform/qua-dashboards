fd4367350605f68cdc6d1428f27ca46403f5e76a
Maxim V4S <maxim.d@v4scale.com>
2025-02-03 11:54:30 +0300

    check3: use func for getting app

--- src/qua_dashboards/app.py
+++ src/qua_dashboards/app.py
@@ -1,52 +1,50 @@
+from typing import Optional
+
 import dash
 from dash import Dash, html, dcc
-from qua_dashboards.logging_config import logger
-from qua_dashboards.pages import dashboards_registry
 import dash_bootstrap_components as dbc
 
 
+def create_app(requests_pathname_prefix: Optional[str] = None) -> Dash:
+    kwargs = {}
+    if requests_pathname_prefix is not None:
+        kwargs["requests_pathname_prefix"] = "/dashboards/"
+    app = Dash(
+        __name__,
+        use_pages=True,
+        external_stylesheets=[dbc.themes.BOOTSTRAP],
+        **kwargs,
+    )
 
-app = Dash(
-    __name__,
-    use_pages=True,
-    external_stylesheets=[dbc.themes.BOOTSTRAP],
-    url_base_pathname="/dashboards/",
-)
+    dash.register_page("another_home", layout=html.Div("We're home!"),
+                       path="/other-path")
+    dash.register_page(
+        "very_important", layout=html.Div("Don't miss it!"), path="/important",
+        order=0
+    )
 
-dash.register_page("another_home", layout=html.Div("We're home!"), path="/other-path")
-dash.register_page(
-    "very_important", layout=html.Div("Don't miss it!"), path="/important", order=0
-)
-
-# for key, dashboard in dashboards_registry.items():
-#     logger.info(f"Setting up dashboard: {key}")
-#     dashboard.setup_api(app=app)
-#     dashboard.setup_callbacks(app=app)
-
-# app.layout = html.Div(
-#     [
-#         dash.page_container,
-#     ]
-# )
-app.layout = html.Div(
-    [
-        html.H1("App Frame"),
-        html.Div(
-            [
-                html.Div(
-                    dcc.Link(
-                        f"{page['name']} - {page['path']}", href=(page["relative_path"])
+    app.layout = html.Div(
+        [
+            html.H1("App Frame"),
+            html.Div(
+                [
+                    html.Div(
+                        dcc.Link(
+                            f"{page['name']} - {page['path']}",
+                            href=(page["relative_path"])
+                        )
                     )
-                )
-                for page in dash.page_registry.values()
-                if page["module"] != "pages.not_found_404"
-            ]
-        ),
-        html.Hr(),
-        dash.page_container,
-    ]
-)
+                    for page in dash.page_registry.values()
+                    if page["module"] != "pages.not_found_404"
+                ]
+            ),
+            html.Hr(),
+            dash.page_container,
+        ]
+    )
+    return app
 
 
 if __name__ == "__main__":
+    app = create_app()
     app.run(debug=True)

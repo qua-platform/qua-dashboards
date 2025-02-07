a8330b77667ac5ada1859a22b5853ab5718e290a
Serwan Asaad <serwan.asaad@quantum-machines.co>
2025-02-07 09:34:10 +0100

    add data dashboard to init

---
+++ src/qua_dashboards/data_dashboard/__init__.py
@@ -0,0 +1,4 @@
+from qua_dashboards.data_dashboard.data_dashboard_client import DataDashboardClient
+from qua_dashboards.data_dashboard.data_dashboard_app import DataDashboardApp
+
+__all__ = ["DataDashboardClient", "DataDashboardApp"]

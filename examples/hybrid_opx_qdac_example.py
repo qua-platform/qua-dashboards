0d2457c6752ff78b3255c4760bdda502ca1fd796
KU-QM <kalid.ulas@quantum-machines.co>
2026-02-09 11:31:27 +0000

    hybrid video mode example refactor

--- examples/hybrid_opx_qdac_example.py
+++ examples/hybrid_opx_qdac_example.py
@@ -74,130 +74,26 @@ from quam_builder.architecture.quantum_dots.components import (
 from quam_builder.architecture.quantum_dots.qpu import BaseQuamQD
 from qua_dashboards.virtual_gates import VirtualLayerEditor, ui_update
 from qua_dashboards.voltage_control import VoltageControlComponent
+from qua_dashboards.utils import setup_DC_channel, setup_readout_channel, connect_to_qdac
 
-
-def setup_DC_channel(
-    name: str, opx_output_port: int, qdac_port: int, opx_trigger_out: int, con="con1", fem: int = None, 
+def setup_DC_channel_with_trigger(
+    name: str, opx_output_port: int, qdac_port: int, opx_trigger_out: int, con="con1", fem: int = None,
 ):
-    """
-    Set up a DC Channel
-
-    Args:
-        name: The channel name in your Quam.
-        opx_ouput_port: The integer output port of your OPX.
-        qdac_port: Integer Qdac output port.
-        con: QM cluster controller, defaults to "con1".
-        fem: If using an OPX1000, add integer FEM number. Defaults to None for OPX+.
-    """
-    if fem is None:
-        opx_output = OPXPlusAnalogOutputPort(
-            controller_id=con,
-            port_id=opx_output_port,
-        )
-    else:
-        opx_output = LFFEMAnalogOutputPort(
-            controller_id=con,
-            fem_id=fem,
-            port_id=opx_output_port,
-            upsampling_mode="pulse",
-        )
-
-    channel = VoltageGate(
-        id=name,
-        opx_output=opx_output,  # Output for channel
-        sticky=StickyChannelAddon(duration=1_000, digital=False),  # For DC offsets
-        attenuation = 10,
+    ch = setup_DC_channel(
+        name = name, 
+        opx_output_port=opx_output_port,
+        qdac_port = qdac_port,
+        con = con, 
+        fem = fem,
     )
     digital_trigger = Channel(
         id = f"{name}_qdac_trigger", 
         digital_outputs={"trigger": DigitalOutputChannel(opx_output = ("con1", fem, opx_trigger_out), delay = 0, buffer = 0)}, 
         operations = {"trigger": pulses.Pulse(length = 100, digital_marker = "ON")}
     )
-    qdac_spec = QdacSpec(qdac_output_port = qdac_port, opx_trigger_out=digital_trigger)
+    ch.qdac_spec.opx_trigger_out = digital_trigger
 
-    channel.qdac_spec = qdac_spec
-    if qdac_port is None:
-        channel.offset_parameter = None
-    return channel
-
-
-def setup_readout_channel(
-    name: str,
-    readout_pulse: pulses.ReadoutPulse,
-    opx_output_port: int,
-    opx_input_port: int,
-    IF: float,
-    con="con1",
-    fem: int = None,
-):
-    """
-    Set up a Readout Channel
-
-    Args:
-        name: The channel name in your Quam.
-        readout_pulse: The Readout Pulse object to be passed to the OPXDataAcquirer.
-        opx_ouput_port: The integer output port of your OPX.
-        opx_input_port: The integer input port of your OPX.
-        IF: The intermediate frequency of your Readout channel.
-        con: QM cluster controller, defaults to "con1".
-        fem: If using an OPX1000, add integer FEM number. Defaults to None for OPX+. Assumed same FEM for output and input channels.
-    """
-
-    if fem is None:
-        opx_output = OPXPlusAnalogOutputPort(
-            controller_id=con,
-            port_id=opx_output_port,
-        )
-        opx_input = OPXPlusAnalogInputPort(
-            controller_id=con,
-            port_id=opx_input_port,
-        )
-    else:
-        opx_output = LFFEMAnalogOutputPort(
-            controller_id=con,
-            fem_id=fem,
-            port_id=opx_output_port,
-            upsampling_mode="mw",
-        )
-        opx_input = LFFEMAnalogInputPort(
-            controller_id=con,
-            fem_id=fem,
-            port_id=opx_input_port,
-        )
-
-    channel = ReadoutResonatorSingle(
-        id=name,
-        opx_output=opx_output,  # Output for the readout pulse
-        opx_input=opx_input,  # Input for acquiring the measurement signal
-        intermediate_frequency=IF,  # Set IF for the readout channel
-        operations={
-            "readout": readout_pulse
-        },  # Assign the readout pulse to this channel
-        time_of_flight=28,
-    )
-    return channel
-
-
-def define_DC_params(machine: QuamRoot, gate_names: List[str]):
-    """
-    Defines gates using QDAC and a channel mapping dict. Provide a list of channel names existing in your Quam object instance.
-
-    Currently assumes VoltageGate objects, using 'offset_parameter" attribute.
-    """
-    from qcodes.parameters import DelegateParameter
-
-    voltage_parameters = []
-    for ch_name in gate_names:
-        ch = machine.physical_channels[ch_name]
-        parameter = getattr(ch, "offset_parameter", None)
-        if parameter is not None:
-            voltage_parameters.append(
-                DelegateParameter(
-                    name=ch_name, label=ch_name, source=ch.offset_parameter
-                )
-            )
-    return voltage_parameters
-
+    return ch
 
 def main():
     logger = setup_logging(__name__)
@@ -224,14 +120,14 @@ def main():
     fem = 5
 
     # Set up the DC channels
-    p1 = setup_DC_channel(name="plunger_1", opx_output_port=1, qdac_port=1, fem=fem, opx_trigger_out = 1)
-    p2 = setup_DC_channel(name="plunger_2", opx_output_port=2, qdac_port=2, fem=fem, opx_trigger_out = 1)
-    p3 = setup_DC_channel(name="plunger_3", opx_output_port=3, qdac_port=3, fem=fem, opx_trigger_out = 1)
-    p4 = setup_DC_channel(name="plunger_4", opx_output_port=8, qdac_port=8, fem=fem, opx_trigger_out = 1)
-    s1 = setup_DC_channel(name="sensor_1", opx_output_port=4, qdac_port=4, fem=fem, opx_trigger_out = 1)
-    s2 = setup_DC_channel(name="sensor_2", opx_output_port=5, qdac_port=5, fem=fem, opx_trigger_out = 1)
-    b1 = setup_DC_channel(name="barrier_1", opx_output_port=6, qdac_port=6, fem=fem, opx_trigger_out = 1)
-    b2 = setup_DC_channel(name="barrier_2", opx_output_port=7, qdac_port=7, fem=fem, opx_trigger_out = 1)
+    p1 = setup_DC_channel_with_trigger(name="plunger_1", opx_output_port=1, qdac_port=1, fem=fem, opx_trigger_out = 1)
+    p2 = setup_DC_channel_with_trigger(name="plunger_2", opx_output_port=2, qdac_port=2, fem=fem, opx_trigger_out = 1)
+    p3 = setup_DC_channel_with_trigger(name="plunger_3", opx_output_port=3, qdac_port=3, fem=fem, opx_trigger_out = 1)
+    p4 = setup_DC_channel_with_trigger(name="plunger_4", opx_output_port=8, qdac_port=8, fem=fem, opx_trigger_out = 1)
+    s1 = setup_DC_channel_with_trigger(name="sensor_1", opx_output_port=4, qdac_port=4, fem=fem, opx_trigger_out = 1)
+    s2 = setup_DC_channel_with_trigger(name="sensor_2", opx_output_port=5, qdac_port=5, fem=fem, opx_trigger_out = 1)
+    b1 = setup_DC_channel_with_trigger(name="barrier_1", opx_output_port=6, qdac_port=6, fem=fem, opx_trigger_out = 1)
+    b2 = setup_DC_channel_with_trigger(name="barrier_2", opx_output_port=7, qdac_port=7, fem=fem, opx_trigger_out = 1)
 
     # Set up the readout channels
     sensor_readout_channel_1 = setup_readout_channel(

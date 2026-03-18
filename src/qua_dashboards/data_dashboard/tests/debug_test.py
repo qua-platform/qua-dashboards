428c5db0994dcbbea91bd40c320460c6eac15964
natangqm <natan.gold@quantum-machines.co>
2026-03-18 08:15:08 +0200

    apply fix

--- src/qua_dashboards/data_dashboard/tests/debug_test.py
+++ src/qua_dashboards/data_dashboard/tests/debug_test.py
@@ -5,5 +5,6 @@ logging.basicConfig(
     level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s"
 )
 
-data_dashboard_app = DataDashboardApp(update_interval=60000, update_button=True)
-data_dashboard_app.app.run_server(debug=True)
+if __name__ == "__main__":
+    data_dashboard_app = DataDashboardApp(update_interval=60000, update_button=True)
+    data_dashboard_app.app.run(debug=True)
--- src/qua_dashboards/utils/example_utils/__init__.py
+++ src/qua_dashboards/utils/example_utils/__init__.py
@@ -1,9 +1,18 @@
-from .quam_utils import setup_DC_channel, setup_readout_channel
 from .voltage_control_utils import define_DC_params, connect_to_qdac
 
 __all__ = [
     "setup_DC_channel",
-    "setup_readout_channel", 
-    "define_DC_params", 
-    "connect_to_qdac", 
+    "setup_readout_channel",
+    "define_DC_params",
+    "connect_to_qdac",
 ]
+
+
+def setup_DC_channel(*args, **kwargs):
+    from .quam_utils import setup_DC_channel as _setup_DC_channel
+    return _setup_DC_channel(*args, **kwargs)
+
+
+def setup_readout_channel(*args, **kwargs):
+    from .quam_utils import setup_readout_channel as _setup_readout_channel
+    return _setup_readout_channel(*args, **kwargs)
